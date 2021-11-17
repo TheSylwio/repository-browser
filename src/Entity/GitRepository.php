359f0ff724d74a40592edc5e96c0e3f53b9d5f7f
TheSylwio <thesylwio@gmail.com>
2021-11-18 00:25:05 +0100

    Create GitRepository entity

---
+++ src/Entity/GitRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+class GitRepository {
+	private string $name;
+	private int $pullRequestsCount;
+	private int $commitCount;
+	private int $starsCount;
+
+	public function getName(): string {
+		return $this->name;
+	}
+
+	public function setName(string $name): static {
+		$this->name = $name;
+		return $this;
+	}
+
+	public function getPullRequestsCount(): int {
+		return $this->pullRequestsCount;
+	}
+
+	public function setPullRequestsCount(int $pullRequestsCount): static {
+		$this->pullRequestsCount = $pullRequestsCount;
+		return $this;
+	}
+
+	public function getCommitCount(): int {
+		return $this->commitCount;
+	}
+
+	public function setCommitCount(int $commitCount): static {
+		$this->commitCount = $commitCount;
+		return $this;
+	}
+
+	public function getStarsCount(): int {
+		return $this->starsCount;
+	}
+
+	public function setStarsCount(int $starsCount): static {
+		$this->starsCount = $starsCount;
+		return $this;
+	}
+}
