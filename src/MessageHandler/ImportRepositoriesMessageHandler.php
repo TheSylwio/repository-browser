c4ff39f5f55a9f140c4b0371f51c81b838ea4d3b
TheSylwio <thesylwio@gmail.com>
2021-11-18 00:27:43 +0100

    Fetch repositories using service

--- src/MessageHandler/ImportRepositoriesMessageHandler.php
+++ src/MessageHandler/ImportRepositoriesMessageHandler.php
@@ -3,10 +3,31 @@
 namespace App\MessageHandler;
 
 use App\Message\ImportRepositoriesMessage;
+use App\Service\GitHubService;
+use Exception;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 final class ImportRepositoriesMessageHandler implements MessageHandlerInterface {
+	private GitHubService $githubService;
+
+	public function __construct(GitHubService $githubService) {
+		$this->githubService = $githubService;
+	}
+
+	/**
+	 * @throws TransportExceptionInterface
+	 * @throws DecodingExceptionInterface
+	 * @throws Exception
+	 */
 	public function __invoke(ImportRepositoriesMessage $message) {
-		echo sprintf('Requested import %s repositories from %s organisation', $message->getProvider(), $message->getOrganisation());
+		switch ($message->getProvider()) {
+			case 'GitHub':
+				$this->githubService->fetchRepositories($message->getOrganisation());
+				break;
+			default:
+				throw new Exception('Passed incorrect git provider');
+		}
 	}
 }
---
+++ src/Service/GitHubService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\GitRepository;
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GitHubService {
+	private HttpClientInterface $client;
+
+	public function __construct(HttpClientInterface $client) {
+		$this->client = $client->withOptions([
+			'base_uri' => 'https://api.github.com',
+			'headers' => [
+				'User-Agent' => 'request',
+			]
+		]);
+	}
+
+	/**
+	 * @throws TransportExceptionInterface
+	 * @throws Exception
+	 * @throws DecodingExceptionInterface
+	 */
+	public function fetchRepositories(string $organisation) {
+		$this->checkOrganisation($organisation);
+
+		$repositories = [];
+		$page = 0;
+
+		do {
+			$response = $this->client->request('GET', sprintf('/orgs/%s/repos?per_page=100&page=%s', $organisation, ++$page));
+			$fetchedRepositories = $response->toArray();
+			$repositories = array_merge($fetchedRepositories, $repositories);
+		} while (count($fetchedRepositories) > 0);
+
+		$repositories = array_map(function ($repository) {
+			return (new GitRepository())
+				->setName($repository['name'])
+				->setCommitCount($this->getRepositoryCommitCount($repository))
+				->setPullRequestsCount(0)
+				->setStarsCount($repository['stargazers_count']);
+		}, $repositories);
+	}
+
+	/**
+	 * @throws TransportExceptionInterface
+	 * @throws Exception
+	 */
+	private function checkOrganisation(string $organisation): void {
+		$response = $this->client->request('HEAD', sprintf('/orgs/%s', $organisation));
+		if ($response->getStatusCode() === Response::HTTP_NOT_FOUND) {
+			throw new Exception(sprintf('Organisation %s not found.', $organisation));
+		}
+	}
+
+	/**
+	 * @throws TransportExceptionInterface
+	 * @throws ServerExceptionInterface
+	 * @throws RedirectionExceptionInterface
+	 * @throws DecodingExceptionInterface
+	 * @throws ClientExceptionInterface
+	 */
+	private function getRepositoryCommitCount(array $repository) {
+		$page = 0;
+		$commitCount = 0;
+
+		do {
+			$response = $this->client->request('GET', sprintf('/repos/%s/contributors?per_page=100&page=%s', $repository['full_name'], ++$page));
+			$contributors = $response->toArray();
+			$commitCount += array_reduce($contributors, function ($acc, $item) {
+				return $acc + $item['contributions'];
+			});
+		} while (count($contributors) > 0);
+
+		return $commitCount;
+	}
+}
